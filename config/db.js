03b953f21da1f5b117946f9757131ece07600176
akshaypatel99 <akshaypatel99@gmail.com>
2022-12-05 13:29:33 +0000

    Remove mongodb config options

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
 	try {
-		const connect = await mongoose.connect(process.env.MONGO_URI, {
-			useUnifiedTopology: true,
-			useNewUrlParser: true,
-			useCreateIndex: true,
-		});
+		const connect = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`MongoDB Connected: ${connect.connection.host}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
